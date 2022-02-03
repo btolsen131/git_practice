e8f67b820e1b5d4d4e860388c9050fbbd7c456e6
Brian T Olsen <btolsen131@gmail.com>
2022-02-03 09:45:30 -0500

    Adding random show generator

---
+++ mixedMessages.js
@@ -0,0 +1,23 @@
+// Random show to watch generator
+
+//creating lists of platforms, genres, and film styles
+let platforms = ['Netflix', 'Hulu','Disney+', 
+        'YoutubeTV', 'HBOMax', 'Discovery+',
+        'Amazon Prime', 'AppleTV'];
+let genres = ['Comedy', 'Drama', 'Thriller',
+            'Documentary', 'Science-Fiction',
+            'Action', 'Fantasy', 'Rom Com',
+            'Reality', 'Western', 'Mystery',
+            'Anime', 'Police Procedural','Game show' ];
+let styles = ['TV Show', 'Big Budget Movie', 'Indie Movie', 'Old movie', 
+            'New movie', 'Old show', 'New Show', ''];
+
+ // Creating function to randomly select from the lists of platforms, genres, and styles.
+function randomShow () {
+    let platform = platforms[Math.floor(Math.random() * platforms.length)];
+    let genre = genres[Math.floor(Math.random() * genres.length)];
+    let style = styles[Math.floor(Math.random() * styles.length)];
+    console.log(`Let's watch a ${genre} ${style} on ${platform}`);
+};
+
+randomShow();
